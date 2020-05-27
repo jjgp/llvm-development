4bab0e470daa23951bc5ba633d101813a2ca6c91
Jason Prasad <jasongprasad@gmail.com>
2020-05-27 19:19:13 -0400

    Shrinks size of image

--- docker/install_llvm.sh
+++ docker/install_llvm.sh
@@ -4,12 +4,24 @@
 
 set -ex
 
+buildDeps='libedit-dev libncurses5-dev python-dev swig wget'
+apt-get update
+apt-get install -y --no-install-recommends \
+  $buildDeps \
+  automake \
+  build-essential \
+  ca-certificates \
+  cmake \
+  gdb \
+  python \
+  valgrind && \
+  rm -rf /var/lib/apt/lists/*
+
 llvm_prefix=/usr/local/lib/llvm-${LLVM_VERSION}
 llvm_source_url="https://github.com/llvm/llvm-project/archive/llvmorg-${LLVM_VERSION}.tar.gz"
 
-mkdir $llvm_prefix
-
 pushd /tmp
+
 wget --no-verbose --output-document=llvm.tar.gz "$llvm_source_url"
 mkdir llvm
 tar zxf llvm.tar.gz -C llvm --strip-components 1
@@ -24,12 +36,18 @@ cmake -G "Unix Makefiles" \
   -DLLVM_ENABLE_PROJECTS="clang;lldb" \
   -DCMAKE_INSTALL_PREFIX=$llvm_prefix \
   -DLLVM_TARGETS_TO_BUILD="host" \
+  -DLLVM_BUILD_TOOLS=OFF \
+  -DLLVM_BUILD_UTILS=OFF \
   ../llvm
 
 make -j4
-sudo make install
-rm -rf llvm
+make install
 
 popd
 
+rm -rf /tmp/llvm
+
 ln -s ${llvm_prefix}/bin/* /usr/bin
+
+apt-get --purge remove -y \
+  $buildDeps
