acd3b496022c6867e235f39c7375abdb69f751ad
Jason Prasad <jasongprasad@gmail.com>
2020-05-28 08:52:39 -0400

    Re-enables build tools / utils

--- docker/install_llvm.sh
+++ docker/install_llvm.sh
@@ -17,7 +17,6 @@ apt-get install -y --no-install-recommends \
   valgrind && \
   rm -rf /var/lib/apt/lists/*
 
-llvm_prefix=/usr/local/lib/llvm-${LLVM_VERSION}
 llvm_source_url="https://github.com/llvm/llvm-project/archive/llvmorg-${LLVM_VERSION}.tar.gz"
 
 pushd /tmp
@@ -34,20 +33,15 @@ cmake -G "Unix Makefiles" \
   -DCMAKE_BUILD_TYPE=MinSizeRel \
   -DLLVM_ENABLE_ASSERTIONS=ON \
   -DLLVM_ENABLE_PROJECTS="clang;lldb" \
-  -DCMAKE_INSTALL_PREFIX=$llvm_prefix \
   -DLLVM_TARGETS_TO_BUILD="host" \
-  -DLLVM_BUILD_TOOLS=OFF \
-  -DLLVM_BUILD_UTILS=OFF \
   ../llvm
 
-make -j4
-make install
+cmake --build . -- -j4
+cmake --build . --target install
 
 popd
 
 rm -rf /tmp/llvm
-
-ln -s ${llvm_prefix}/bin/* /usr/bin
 
 apt-get --purge remove -y \
   $buildDeps
